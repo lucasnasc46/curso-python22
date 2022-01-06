ef20baf91b23e2beeac4b9f0df8dab8534444ac3
Lucas Nascimento <lucasthuba@gmail.com>
2022-01-06 20:52:02 -0300

    Feito questao de verificar idade

---
+++ verifica_idade.py
@@ -0,0 +1,9 @@
+idade = int(input("digite seu ano de nascimento"))
+
+cal = 2022-idade
+
+if cal < 18:
+    print("sua idade é:",cal, "voce é menor de idade")
+
+if cal >= 18:
+    print("sua idade é",cal, "voce é maio de idade")    
