32974f7b0d6a27a9e9bb633ce385d872005490e3
Lucas Nascimento <lucasthuba@gmail.com>
2022-01-06 16:56:06 -0300

    Feito exercicio de conversão de moeda durante a aula

---
+++ conversor_moeda.py
@@ -0,0 +1,8 @@
+#Conversor de moeda, dollar para real
+
+#Solcintando o valor para conversao e definindo o input como float para trabalhar com casas decimais e sakvando na variavel dollar
+dollar = float(input("Digite quantos dollares vc quer converter para real")) 
+
+convert = dollar*5.68 # fazendo a conversão e salvando o valor na variavel convert
+
+print(dollar,"dollares equivale a R$", convert) #mostrando o resultado da conversão
