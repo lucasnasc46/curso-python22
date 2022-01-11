16d8fc8ea16eeddbf71ff02cca96e2839314ae6d
Lucas Nascimento <lucasthuba@gmail.com>
2022-01-11 20:49:49 -0300

    Ajustado if

--- calcula_func.py
+++ calcula_func.py
@@ -1,5 +1,6 @@
 #calculadora usando função
 
+#Definindo as funções de cada operação
 def soma (n1, n2):
     return n1 + n2
 
@@ -21,14 +22,14 @@ if op == "+":
     valor = soma(n1,n2)
     print(f"a soma de {n1} + {n2} e:", valor)
 
-if op == "-":
+elif op == "-":
     valor = sub(n1,n2)
     print(f"a soma de {n1} - {n2} e:", valor)
 
-if op == "*" :
+elif op == "*" :
     valor = mult(n1,n2)
     print(f"a soma de {n1} * {n2} e:", valor)
 
-if op == "/" :
+elif op == "/" :
     valor = div(n1,n2)
     print(f"a soma de {n1} / {n2} e:", valor)
