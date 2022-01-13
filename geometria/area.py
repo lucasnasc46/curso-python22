7dc7e1fd1b6ff7440d326d751cdf18b4339bf079
Lucas Nascimento <lucasthuba@gmail.com>
2022-01-13 14:26:51 -0300

    feito questao da geometrica com modulo

---
+++ geometria/area.py
@@ -0,0 +1,10 @@
+#Modulo para calcular a area de um ciculo e um quandrado
+
+#Definindo as funçoes para calculo de cada figura geometrica
+def circulo (raio):
+    area= 3.14 * raio ** 2
+    return area
+
+def quadrado(L):
+    lado= L**2
+    return lado    
